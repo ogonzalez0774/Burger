2c9d4cb66696db3baed9652004b1096201a85967
Oscar Gonzalez <ogonzalez0774@gmail.com>
2019-11-01 15:58:36 -0500

    fixed error in seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
-INSERT INTO burgers(burger_name) VALUES("Bacon Cheeseburger")
+INSERT INTO burgers(burger_name) VALUES("Bacon Cheeseburger");
 
-INSERT INTO burgers(burger_name) VALUES("CheeseBurger")
+INSERT INTO burgers(burger_name) VALUES("CheeseBurger");
 
 INSERT INTO burgers(burger_name) VALUES("Hamburger")
